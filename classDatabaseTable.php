2d2da279976e309e957cca4f4afc6230ea13595b
scott <scott@ubuntuServer.(none)>
2010-05-31 12:48:50 -0400

    Cleaned up display of database tables.

--- classDatabaseTable.php
+++ classDatabaseTable.php
@@ -7,18 +7,20 @@ class classDatabaseTable extends Parseable{
 	}
 	
 	public function ToHTML(){
-		$strData = '<table border="1">' . PHP_EOL;
+		$strData = '<table class="classDatabaseTable">' . PHP_EOL;
 		// print the table header
-		$strData .= '<tr><td>Field</td><td>Type</td><td>Null</td><td>Key</td><td>Default</td><td>Extra</td></tr>' . PHP_EOL;
+		$strData .= '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default</th><th>Extra</th></tr>' . PHP_EOL;
 		
 		// loop through all the arrays
 		foreach($this->_arrData as $arrLine){
 			// create a line to display this
 			$strData .= '<tr><td>'.$arrLine[0].'</td><td>'.$arrLine[1].'</td><td>'.$arrLine[2].'</td><td>'.$arrLine[3].'</td><td>'.$arrLine[4].'</td><td>'.$arrLine[5].'</td></tr>' . PHP_EOL;
 		}
-		
-		
+				
 		$strData .= '</table>' . PHP_EOL;
+		
+		
+		
 		return $strData;
 	}
 }
